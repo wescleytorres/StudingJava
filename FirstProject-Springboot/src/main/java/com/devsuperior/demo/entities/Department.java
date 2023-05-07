c9389b6b22c0e64f85a0253ad2e8e8f95378115b
WescleyTorres <jewescley@gmail.com>
2023-05-07 08:33:01 -0300

    BD with ORM - JAVA and Spring

--- FirstProject-Springboot/src/main/java/com/devsuperior/demo/entities/Department.java
+++ FirstProject-Springboot/src/main/java/com/devsuperior/demo/entities/Department.java
@@ -1,6 +1,17 @@
 package com.devsuperior.demo.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_department")
 public class Department {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long Id;
   private String name;
 

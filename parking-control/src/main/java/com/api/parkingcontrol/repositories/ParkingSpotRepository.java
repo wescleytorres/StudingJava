e5bb129502a410c1ce1c7788341fd72db9b1aea3
WescleyTorres <jewescley@gmail.com>
2023-05-08 07:33:13 -0300

    Parking Control - Spring boot and Postgres

---
+++ parking-control/src/main/java/com/api/parkingcontrol/repositories/ParkingSpotRepository.java
@@ -0,0 +1,16 @@
+package com.api.parkingcontrol.repositories;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.api.parkingcontrol.models.ParkingSpotModel;
+
+public interface ParkingSpotRepository extends JpaRepository<ParkingSpotModel, UUID> {
+
+  boolean existsByLicensePlateCar(String licensePlateCar);
+
+  boolean existsByParkingSpotNumber(String parkingSpotNumber);
+
+  boolean existsByApartmentAndBlock(String apartment, String block);
+}

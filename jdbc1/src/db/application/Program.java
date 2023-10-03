501015ebc5ede9352802072e7e13d834f0bfa247
WescleyTorres <jewescley@gmail.com>
2023-10-03 20:13:31 -0300

    DB com JDBC

---
+++ jdbc1/src/db/application/Program.java
@@ -0,0 +1,13 @@
+package db.application;
+
+import java.sql.Connection;
+
+import db.DB;
+
+public class Program {
+
+  public static void main(String[] args) {
+    Connection conn = DB.getConnection();
+    DB.closeConnection();
+  }
+}

121c2c66f01aa2bca50de00e54f2b5e7f29b3783
WescleyTorres <jewescley@gmail.com>
2023-10-04 01:08:36 -0300

    CRUD com JDBC

--- jdbc1/src/application/Program.java
+++ jdbc1/src/application/Program.java
@@ -1,23 +1,24 @@
 package application;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
+// import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.sql.ResultSet;
-// import java.sql.Statement;
+// import java.text.ParseException;
+// import java.text.SimpleDateFormat;
+// import java.sql.ResultSet;
 
 import db.DB;
-import db.DbIntegrityException;
+import db.DbException;
+// import db.DbIntegrityException;
 
 public class Program {
 
   public static void main(String[] args) {
 
     Connection conn = null;
-    PreparedStatement st = null;
+    // PreparedStatement st = null;
+    Statement st = null;
 
     /*
      * ---------------------------- RECUPERACAO DE DADOS
@@ -121,32 +122,71 @@ public class Program {
     // DB.closeConnection();
     // }
 
-    // * --------------------------- DELETE DE DADOS -----------------------------
-    // */
+    /* --------------------------- DELETE DE DADOS ----------------------------- */
+
+    // try {
+    // conn = DB.getConnection();
+
+    // st = conn.prepareStatement(
+    // "DELETE FROM department "
+    // + "WHERE "
+    // + "Id = ?");
+
+    // st.setInt(1, 2);
+
+    // int rowsAffected = st.executeUpdate();
+
+    // System.out.println("Done! Rows affected: " + rowsAffected);
+
+    // } catch (SQLException e) {
+
+    // throw new DbIntegrityException(e.getMessage());
+
+    // } finally {
+
+    // DB.closeStatement(st);
+    // DB.closeConnection();
+
+    // }
+
+    /*
+     * --------------------------- TRANSACTION DE DADOS
+     * -----------------------------
+     */
 
     try {
       conn = DB.getConnection();
 
-      st = conn.prepareStatement(
-          "DELETE FROM department "
-              + "WHERE "
-              + "Id = ?");
+      // a transacao sera confirmada manualmente
+      conn.setAutoCommit(false);
 
-      st.setInt(1, 2);
+      st = conn.createStatement();
 
-      int rowsAffected = st.executeUpdate();
+      int rows1 = st.executeUpdate("UPDATE seller SET BaseSalary = 2090 WHERE DepartmentId = 1");
 
-      System.out.println("Done! Rows affected: " + rowsAffected);
+      // int x = 1;
+      // if (x < 2) {
+      // throw new SQLException("Fake error");
+      // }
 
+      int rows2 = st.executeUpdate("UPDATE seller SET BaseSalary = 3090 WHERE DepartmentId = 2");
+
+      // informando confirmacao
+      conn.commit();
+
+      System.out.println("rows1 = " + rows1);
+      System.out.println("rows2 = " + rows2);
     } catch (SQLException e) {
-
-      throw new DbIntegrityException(e.getMessage());
-
+      try {
+        // a transacao retorna
+        conn.rollback();
+        throw new DbException("Transaction rolled back! Caused by: " + e.getMessage());
+      } catch (SQLException e1) {
+        throw new DbException("Error trying to rollback! Caused by: " + e1.getMessage());
+      }
     } finally {
-
       DB.closeStatement(st);
       DB.closeConnection();
-
     }
 
   }

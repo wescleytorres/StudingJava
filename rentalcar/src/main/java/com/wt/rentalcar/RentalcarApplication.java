559722ef1e0360ee4ced3f7e1699c7bf38eb3283
WescleyTorres <jewescley@gmail.com>
2023-10-09 12:06:04 -0300

    Rental Car Studying API REST

---
+++ rentalcar/src/main/java/com/wt/rentalcar/RentalcarApplication.java
@@ -0,0 +1,13 @@
+package com.wt.rentalcar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalcarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalcarApplication.class, args);
+	}
+
+}
---
+++ rentalcar/src/test/java/com/wt/rentalcar/RentalcarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wt.rentalcar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalcarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
